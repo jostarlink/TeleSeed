205a7f78ff517e0cc9c6ac499fa3e600507461c1
jostarlink <dideno.group1@gmail.com>
2017-02-14 18:43:21 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,19 +220,19 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {110626080,103649648,0,tonumber(our_id)},--Sudo users
+    sudo_users = {350664620,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/jostarlink/TeleSeed
 
 Admins
 @iwals [Founder]
 @imandaneshi [Developer]
-@seyedan25 [Manager]
+@ms00sm [Manager]
 
 Special thanks to
 awkward_potato
